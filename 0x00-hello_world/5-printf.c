40ad28e40d59c4cc81f193312cd64b652e2f0bfa
Dacoder01 <davidnxumalo32@gmail.com>
2023-08-14 18:35:42 -0700

    5.Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main -this is a main function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
