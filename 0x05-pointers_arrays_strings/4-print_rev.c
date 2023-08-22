8745024765fd1b409c2ef2fdd80262e8324000d5
Dacoder01 <davidnxumalo32@gmail.com>
2023-08-22 01:52:29 -0700

    4.print the string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev - print the string in reverse
+ *@s: function parameter
+ * Return:void
+ */
+void print_rev(char *s)
+{
+	int i;
+	int length = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+		length++;
+	for (i = length - 1; i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
